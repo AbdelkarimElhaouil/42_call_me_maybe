b3294e10c69f1f1f76fcc95a9e6bf14920f65312
marshall <karimwins177@gmail.com>
2026-07-01 09:19:08 +0100

    in parsing ...

--- call_me_maybe/main.py
+++ call_me_maybe/main.py
@@ -1,5 +1,12 @@
 import sys
+from parser import Parser
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         input_file = ""
+    prompts = Parser.parse_input_file('../data/input/function_calling_tests.json')
+    functions = Parser.parse_func_file('../data/input/functions_definition.json')
+    # d = {'a':1}
+    for k in prompts:
+        print(k.prompt)
+
--- call_me_maybe/parser.py
+++ call_me_maybe/parser.py
@@ -1,5 +1,5 @@
 from sys import exit
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, model_validator
 import json
 
 
@@ -10,35 +10,70 @@ class FunctionDefinition(BaseModel):
     returns: dict[str, str]
 
     @model_validator(mode="after")
-    def validate_types(self) -> None:
+    def validate_types(self) -> "FunctionDefinition":
         allowed_types = [
             "number", "string", "integer",
             "array", "object"
         ]
         for k in self.parameters.keys():
-            if self.parameters[k] not in allowed_types:
-                print(f"TypeError: {self.parameters[k]} is invalid")
-                exit(1)
-            if self.returns["type"] not in allowed_types:
-                print(f"TypeError: {self.parameters[k]} is invalid")
-                exit(1)
+            if len(self.parameters[k]) != 1:
+                raise ValueError(
+                    f"Parameter '{k}' has invalid structure: expected only 'type' field"
+                )
+            if self.parameters[k]["type"] not in allowed_types:
+                raise ValueError(
+                    f"Parameter '{k}' has invalid type: '{self.parameters[k]['type']}'"
+                )
+        
+        if self.returns["type"] not in allowed_types:
+            raise ValueError("Invalide return type")
+        
+        return self
 
 class Prompt(BaseModel):
-    Prompt: str
+    prompt: str
 
 
 class Parser:
-    # def __init__(self, path:str):
-    #     self.path = path
+    @staticmethod
     def parse_func_file(path: str) -> list[FunctionDefinition]:
+        functions: list[FunctionDefinition] = []
         try:
             with open(path) as file:
                 data = json.load(file)
-                pass
+                functions = [FunctionDefinition(**func) for func in data]
+                return functions
 
         except IOError as e:
             print(f"Error occured while opening file {path}: {e}")
             exit(1)
 
-    def parse_input_file(path: str) -> list[str]:
-        pass
+        except ValidationError as e:
+            print(f"Validation error: {e}")
+            exit(1)
+        except json.JSONDecodeError as e:
+            print(f"Invalid JSON in {path}: {e}")
+            exit(1)
+
+    @staticmethod
+    def parse_input_file(path: str) -> list[Prompt]:
+        prompts: list[Prompt] = []
+
+        try:
+            with open(path) as file:
+                data =  json.load(file)
+                prompts = [Prompt(**prompt) for prompt in data]
+                print(data[2])
+                return prompts
+        
+        except IOError as e:
+            print(f"Error occured while opening file {path}: {e}")
+            exit(1)
+
+        except (ValidationError, KeyError) as e:
+            print(f"Validation error: {e}")
+            exit(1)
+        
+        except json.JSONDecodeError as e:
+            print(f"Invalid JSON in {path}: {e}")
+            exit(1)
